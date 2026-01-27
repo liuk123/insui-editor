19dba0139d58e19cc15428651769d930c4777e70
liuk123 <liuk123@petalmail.com>
2026-01-27 21:53:17 +0800

    1

--- src/app/editor/components/editor/editor.component.ts
+++ src/app/editor/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, inject, OnDestroy, Output, signal, ViewChild, ViewEncapsulation, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, inject, OnDestroy, Output, signal, ViewChild, ViewEncapsulation, Input, Inject, DOCUMENT } from "@angular/core";
 import { injectElement, INS_APPEARANCE_OPTIONS, InsAppearance, InsControl, InsValueTransformer, insGetWordRange, InsBooleanHandler, InsDropdown, InsDropdownOpen, InsDropdownDirective } from "@liuk123/insui";
 import { INS_EDITOR_OPTIONS } from "../../common/editor-options";
 import { InsEditorAttachedFile } from "../../common/attached";
@@ -14,6 +14,9 @@ import { AbstractInsEditor } from "../../common/editor-adapter";
 import { provideInsEditor } from "../../providers/provide-ins-editor";
 import { INS_EDITOR_PROVIDERS } from "./editor.providers";
 import { InsEditorSocket } from "../editor-socket";
+import { InsEditorDropdownToolbar } from "./dropdown/dropdown-toolbar.directive";
+import { insGetSelectionState, InsSelectionState } from "../../directives/tiptap-editor/utils/get-selection-state";
+import { insIsSafeLinkRange } from "../../directives/tiptap-editor/utils/safe-link-range";
 
 
 @Component({
@@ -26,6 +29,7 @@ import { InsEditorSocket } from "../editor-socket";
     InsTiptapEditor,
     InsDropdown,
     InsEditorSocket,
+    InsEditorDropdownToolbar
   ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -62,7 +66,9 @@ export class InsEditor extends InsControl<string> implements OnDestroy {
     InsValueTransformer<string | null, string | null>
   >(INS_EDITOR_VALUE_TRANSFORMER, { optional: true });
 
-  protected readonly insDropdownOpen = inject(InsDropdownOpen, {optional: true});
+  protected readonly insDropdownOpen = inject(InsDropdownOpen, { optional: true });
+
+  private readonly doc: Document | null = inject(DOCUMENT);
 
 
 
@@ -83,9 +89,16 @@ export class InsEditor extends InsControl<string> implements OnDestroy {
   @Output()
   public readonly focusOut = new EventEmitter<void>();
 
+  private hasMentionPlugin = false;
   protected readonly $ = this.editorLoaded$
     .pipe(delay(0), takeUntilDestroyed())
     .subscribe(() => {
+
+      this.hasMentionPlugin = !!this.editorService
+        .getOriginTiptapEditor()
+        ?.extensionManager.extensions.find(
+          (extension) => extension.name === 'mention',
+        );
 
       const processed =
         this.contentProcessor?.fromControlValue(this.control.value) ??
@@ -99,9 +112,9 @@ export class InsEditor extends InsControl<string> implements OnDestroy {
 
   }
 
-public get editor(): AbstractInsEditor | null {
-        return this.editorService.getOriginTiptapEditor() ? this.editorService : null;
-    }
+  public get editor(): AbstractInsEditor | null {
+    return this.editorService.getOriginTiptapEditor() ? this.editorService : null;
+  }
   public get nativeFocusableElement(): HTMLDivElement | null {
     return (
       this.el?.nativeElement.querySelector('[contenteditable].ProseMirror') || null
@@ -121,29 +134,53 @@ public get editor(): AbstractInsEditor | null {
     event.preventDefault();
     this.nativeFocusableElement?.focus();
   }
- 
+
 
   protected get dropdownSelectionHandler(): InsBooleanHandler<Range> {
-    if (!this.focused()||this.readOnly()) {
-      return ()=>false;
+    if (!this.focused() || this.readOnly()) {
+      return () => false;
     }
-    return this.floatingToolbar?
-      (range)=> (this.value().trim() !== '' && !this.editor?.state?.selection.empty)||
-      this.openDropdownWhen(range)
+    return this.floatingToolbar ?
+      (range) => (this.value().trim() !== '' && !this.editor?.state?.selection.empty) ||
+        this.openDropdownWhen(range)
       : this.openDropdownWhen
   }
-  private readonly openDropdownWhen = (range: Range):boolean => 
-    this.predicate(range) ||
+  private readonly openDropdownWhen = (range: Range): boolean =>
+    this.currentFocusedNodeIsTextAnchor(range) ||
+    this.isMentionMode ||
     Boolean(this.insDropdownOpen?.insDropdownOpen);
 
-  protected predicate: InsBooleanHandler<Range> = (value) => {
-    console.log(String(insGetWordRange(value)).startsWith('@'))
-    return String(insGetWordRange(value)).startsWith('@')
+  // protected predicate: InsBooleanHandler<Range> = (value) => {
+  //   console.log(String(insGetWordRange(value)).startsWith('@'))
+  //   return String(insGetWordRange(value)).startsWith('@')
+  // }
+  private get focusNode(): Node | null {
+    return this.doc?.getSelection()?.focusNode ?? null;
   }
-  
+  /**
+     * @description:
+     * The commonAncestorContainer not always relevant node element in Range,
+     * so the focusNode is used for the correct behaviour from the selection,
+     * which is the actual element at the moment
+     */
+  private currentFocusedNodeIsTextAnchor(range: Range): boolean {
+    return (
+      this.focusNode?.nodeName === 'A' ||
+      !!this.focusNode?.parentElement?.closest('a') ||
+      !!this.focusNode?.parentElement?.closest('[insEditorRootEditLink]') ||
+      (!!range.startContainer.parentElement?.closest('a') &&
+        insIsSafeLinkRange(range))
+    );
+  }
 
 
+  public get isMentionMode(): boolean {
+    return this.hasMentionPlugin && this.selectionState.before.startsWith('@');
+  }
 
+  public get selectionState(): InsSelectionState {
+    return insGetSelectionState(this.editor);
+  }
   onModelChange(value: string | null): void {
     if (value === '' && !this.editorLoaded()) {
       return;
@@ -154,12 +191,12 @@ public get editor(): AbstractInsEditor | null {
     }
   }
   protected onActiveZone(focused: boolean): void {
-        this.focused.set(focused);
+    this.focused.set(focused);
 
-        if (focused) {
-            this.focusIn.emit();
-        } else {
-            this.focusOut.emit();
-        }
+    if (focused) {
+      this.focusIn.emit();
+    } else {
+      this.focusOut.emit();
     }
+  }
 }
