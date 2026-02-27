21e5a8f59e9888177c3374bc63ee9588e5e58e45
liuk123 <liukai_0218@qq.com>
2026-02-27 14:23:00 +0800

    1

--- src/app/editor/components/drag-handle-menu/drag-handle-menu.ts
+++ src/app/editor/components/drag-handle-menu/drag-handle-menu.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { InsDataList } from '@liuk123/insui';
 
 @Component({
   selector: 'ins-drag-handle-menu',
-  imports: [],
+  imports: [InsDataList],
   templateUrl: './drag-handle-menu.html',
   styleUrl: './drag-handle-menu.less',
 })
 export class InsDragHandleMenu {
-
+  protected items = [
+    {
+      key: 'remove',
+      icon: 'heading-1',
+      name: '删除',
+    },
+    {
+      key: 'clearFormat',
+      icon: 'heading-1',
+      name: '清除格式',
+    },
+  ];
+  command(item: typeof this.items[0]) {
+    console.log(item);
+  }
 }
